41b5351dd53a9f556d7b6203453b868e70d83333
Alvaro Santa Cruz <alvaro.santacruz@pucp.pe>
2020-04-14 11:37:12 -0500

    Post comments

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -22,7 +22,7 @@ function RenderDish({ dish }) {
     </div>
 }
 
-function RenderComments({ comments, addComment, dishId }) {
+function RenderComments({ comments, postComment, dishId }) {
     if (comments == null) return <div></div>
 
     let commentsView = comments.map((comment) => {
@@ -40,7 +40,7 @@ function RenderComments({ comments, addComment, dishId }) {
         <ul className="list-unstyled">
             {commentsView}
         </ul>
-        <CommentForm dishId={dishId} addComment={addComment} />
+        <CommentForm dishId={dishId} postComment={postComment} />
     </div>
 }
 
@@ -79,7 +79,7 @@ const DishDetail = (props) => {
             <div className="row">
                 <RenderDish dish={props.dish} />
                 <RenderComments comments={props.comments}
-                    addComment={props.addComment}
+                    postComment={props.postComment}
                     dishId={props.dish.id} />
             </div>
         </div>
@@ -103,7 +103,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     toggleModal() {
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { actions } from 'react-redux-form';
 import { Redirect, Route, Switch, withRouter } from 'react-router-dom';
-import { addComment, fetchComments, fetchDishes, fetchPromos } from '../redux/ActionCreators';
+import { postComment, fetchComments, fetchDishes, fetchPromos } from '../redux/ActionCreators';
 import About from './AboutComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
@@ -24,10 +24,10 @@ const mapStateToProps = state => {
 }
 
 const mapDispathToProps = dispatch => ({
-    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
     fetchDishes: () => { dispatch(fetchDishes()) },
     fetchComments: () => dispatch(fetchComments()),
     fetchPromos: () => dispatch(fetchPromos()),
+    postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
     resetFeedbackForm: () => { dispatch(actions.reset('feedback')) },
 })
 
@@ -48,7 +48,7 @@ class Main extends Component {
                     errMess={this.props.dishes.errMess}
                     comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
                     commentsErrMess={this.props.comments.errMess}
-                    addComment={this.props.addComment}
+                    postComment={this.props.postComment}
                 />
             );
         };
